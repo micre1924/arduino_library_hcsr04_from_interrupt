a716417ec57942fc787ee849c6fba810bb73cd7b
micre1924 <michaelreul87@gmail.com>
2022-09-14 14:07:21 +0200

    sync_sf

--- src/hcsr04_from_interrupt.cpp
+++ src/hcsr04_from_interrupt.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include "hcsr04_from_interrupt.h"
 
-void hcsr04_from_interrupt::interruptAction() {
+void hcsr04_from_interrupt::onChangeInterrupt() {
 	if(digitalRead(hcsr04_from_interrupt::echoPin)) {
 		echoStartTimestamp = micros();
 	}
@@ -15,10 +15,10 @@ void hcsr04_from_interrupt::interruptAction() {
 hcsr04_from_interrupt::hcsr04_from_interrupt(int triggerPin, int echoPin ,int initEnvTemp = 22){
 	pinMode(echoPin, INPUT);
 	pinMode(triggerPin, OUTPUT);
-	triggerPin = this->triggerPin;
-	echoPin = this->echoPin;
-	envTemperature = this->envTemperature;
-
+	this->triggerPin = triggerPin;
+	this->echoPin = echoPin;
+	this->envTemperature = initEnvTemp;
+	attachInterrupt(digitalPinToInterrupt(echoPin), hcsr04_from_interrupt::onChangeInterrupt, CHANGE);
 }
 void hcsr04_from_interrupt::trigger(int envTemp){
 	digitalWrite(triggerPin, HIGH);
--- src/hcsr04_from_interrupt.h
+++ src/hcsr04_from_interrupt.h
@@ -1,13 +1,16 @@
 #include "Arduino.h"
 
+// - static members is not the way this is planned, but for temporary function of the library tollerated
+// - there is a temporary limit of one instance of this class because of the interruption management
 class hcsr04_from_interrupt{
 public:
-    int envTemperature = 22;
-    int triggerPin = -1;
-    int echoPin = -1;
-    int distanceMM = -1;
-    unsigned long echoStartTimestamp = 0;
-    unsigned int triggerEchoPeriode = 0;
+    
+    static int envTemperature;
+    static int triggerPin;
+    static int echoPin;
+    static int distanceMM;
+    static unsigned long echoStartTimestamp;
+    static unsigned int triggerEchoPeriode;
 
     /*
     @param triggerPin don't has to be interruptable
@@ -16,5 +19,5 @@ public:
     hcsr04_from_interrupt(int triggerPin, int echoPin ,int initEnvTemp = 22);
     void trigger(int envTemp);
     // set this function as the callback for interrupt on the "echoPin"
-    void interruptAction();
+    static void onChangeInterrupt();
 };
